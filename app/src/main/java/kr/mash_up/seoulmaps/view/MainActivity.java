d32349b23f26173d6d767da72b5de4e3aec91182
Tak <tak8997@nate.com>
2017-07-26 17:15:44 +0900

    splash, googlemap 초기화 작업, butterknife library 추가

--- app/src/main/java/kr/mash_up/seoulmaps/view/MainActivity.java
+++ app/src/main/java/kr/mash_up/seoulmaps/view/MainActivity.java
@@ -1,8 +1,17 @@
 package kr.mash_up.seoulmaps.view;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.UiThread;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import kr.mash_up.seoulmaps.R;
+import kr.mash_up.seoulmaps.base.BaseActivity;
 import kr.mash_up.seoulmaps.present.MainContract;
 import kr.mash_up.seoulmaps.present.MainPresenter;
 
@@ -10,15 +19,53 @@ import kr.mash_up.seoulmaps.present.MainPresenter;
  * Created by wooyoungki on 2017. 7. 24..
  */
 
-public class MainActivity extends AppCompatActivity implements MainContract.View {
+public class MainActivity extends BaseActivity implements MainContract.View, OnMapReadyCallback {
 
     MainContract.Presenter p;
+
+    private GoogleMap map;
 
     @Override
     public void onCreate(Bundle savedBundle){
         super.onCreate(savedBundle);
 
         p = new MainPresenter(this);
+    }
 
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+        setGoogleMap();
+        setUpToolbar();
+    }
+
+    private void setGoogleMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @UiThread
+    private void setUpToolbar() {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        map.addMarker(markerOptions);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
--- app/src/main/java/kr/mash_up/seoulmaps/view/SplashActivity.java
+++ app/src/main/java/kr/mash_up/seoulmaps/view/SplashActivity.java
@@ -1,15 +1,27 @@
 package kr.mash_up.seoulmaps.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 import kr.mash_up.seoulmaps.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
     }
+
 }
