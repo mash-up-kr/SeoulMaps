d83103404ff3e4eab72700d71782a575075aced9
Tak <tak8997@nate.com>
2017-11-26 20:08:25 +0900

    커스텀 아이콘 추가 및 수정. 커스텀 뷰 생성.

--- app/src/main/java/kr/mash_up/seoulmaps/view/CustomStarView.kt
+++ app/src/main/java/kr/mash_up/seoulmaps/view/CustomStarView.kt
@@ -1,29 +1,45 @@
 package kr.mash_up.seoulmaps.view
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.content.res.TypedArray
+import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.five_stars_indicator.view.*
 import kr.mash_up.seoulmaps.R
 
 /**
  * Created by Tak on 2017. 11. 23..
  */
-class CustomStarView(context: Context?) : LinearLayout(context) {
-    private var mStar1: ImageView? = null
-    private var mStar2: ImageView? = null
-    private var mStar3: ImageView? = null
+
+class CustomStarView : LinearLayout {
     private var mSelected = 0
 
-    constructor(context: Context?, attrs: AttributeSet?) : this(context) {
-        initializeView(context, attrs)
+    private lateinit var mStar1 : ImageView
+    private lateinit var mStar2 : ImageView
+    private lateinit var mStar3 : ImageView
+    private lateinit var mStar4 : ImageView
+    private lateinit var mStar5 : ImageView
+
+    constructor(context: Context) : super(context) {
+        initializeView(context, null)
     }
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : this(context, attrs) {
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         initializeView(context, attrs)
     }
 
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initializeView(context, attrs)
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
+        initializeView(context, attrs)
+    }
 
     private fun initializeView(context: Context?, attrs: AttributeSet?) {
         val inflater : LayoutInflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -37,6 +53,12 @@ class CustomStarView(context: Context?) : LinearLayout(context) {
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+
+        mStar1 = findViewById(R.id.star1) as ImageView
+        mStar2 = findViewById(R.id.star2) as ImageView
+        mStar3 = findViewById(R.id.star3) as ImageView
+        mStar4 = findViewById(R.id.star4) as ImageView
+        mStar5 = findViewById(R.id.star5) as ImageView
 
         setSelected(mSelected, true)
     }
@@ -52,35 +74,35 @@ class CustomStarView(context: Context?) : LinearLayout(context) {
             }
             mSelected = select
             if(mSelected == 0) {
-                mStar1?.setImageResource(R.drawable.star)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
+                star1?.setImageResource(R.drawable.star)
+                star2?.setImageResource(R.drawable.star_empty)
+                star3?.setImageResource(R.drawable.star_empty)
+                star4?.setImageResource(R.drawable.star_empty)
+                star5?.setImageResource(R.drawable.star_empty)
             } else if(mSelected == 1) {
-                mStar1?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
+                star1?.setImageResource(R.drawable.star_empty)
+                star2?.setImageResource(R.drawable.star)
+                star3?.setImageResource(R.drawable.star_empty)
+                star4?.setImageResource(R.drawable.star_empty)
+                star5?.setImageResource(R.drawable.star_empty)
             } else if(mSelected == 2) {
-                mStar1?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
+                star1?.setImageResource(R.drawable.star_empty)
+                star2?.setImageResource(R.drawable.star_empty)
+                star3?.setImageResource(R.drawable.star)
+                star4?.setImageResource(R.drawable.star_empty)
+                star5?.setImageResource(R.drawable.star_empty)
             } else if(mSelected == 3) {
-                mStar1?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star)
-                mStar2?.setImageResource(R.drawable.star_empty)
+                star1?.setImageResource(R.drawable.star_empty)
+                star2?.setImageResource(R.drawable.star_empty)
+                star3?.setImageResource(R.drawable.star_empty)
+                star4?.setImageResource(R.drawable.star)
+                star5?.setImageResource(R.drawable.star_empty)
             } else if(mSelected == 4){
-                mStar1?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star_empty)
-                mStar2?.setImageResource(R.drawable.star)
+                star1?.setImageResource(R.drawable.star_empty)
+                star2?.setImageResource(R.drawable.star_empty)
+                star3?.setImageResource(R.drawable.star_empty)
+                star4?.setImageResource(R.drawable.star_empty)
+                star5?.setImageResource(R.drawable.star)
             }
         }
     }
